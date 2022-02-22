d1151c62bf2da71fedf942e156c1f4fe7afc1170
saifrahmn <saifrahmn78@gmail.com>
2022-02-23 00:13:39 +0530

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+#this is the main code of the bot
+import os 
+ 
+
+input("Press any bottom")
+
+condn = input("hi, how are you?")
+if condn=="fine":
+    print("Good to here that..!")
+else:
+    print("Sorry to hear that but I believe that you are gonna make it..!")
