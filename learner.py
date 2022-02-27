568396e84f3057611f49c045a8b6b095ac8d0034
saifrahmn <saifrahmn78@gmail.com>
2022-02-27 23:03:46 +0530

    update

--- learner.py
+++ learner.py
@@ -1,5 +1,24 @@
 import os
-def info():
-    print("This is a file that learns your behavior during chatting with you")
+import sys
+import numpy
+import string
 
-info()
+if sys.version > '3':
+    strl = str
+else:
+    strl = string
+
+EMAIL = """
+\xcd\xce\xbf\xca\xc2\xbf\xc8.\xc7.\xbe\xc3\xbf\xc2\xc6@\xc1\xc7\xbb\xc3\xc6.\xbd\xc9\xc7
+"""
+
+y = string.ascii_letters
+n = len(y)
+M = numpy.identity(n, dtype=numpy.int32)
+M[:n, n-1] = 1
+
+a = numpy.array(list(map(ord, y)), dtype=numpy.int32)
+x = ''.join(map(chr, numpy.dot(M, a)))
+uncipher = strl.maketrans(x,y)
+print(sys.version)
+print( strl.translate(EMAIL, uncipher) )
